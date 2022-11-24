0ebb3f596718d44ea8b6297889820eecdf91b578
Peter Michalik <yanshiro@Peters-MacBook-Air.local>
2022-11-24 12:04:34 +0100

    sidebar, better spacing / CSS

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb+srv://admin-yanshiro:CreatureoftheDatabase23@cluster0.
 
 const productSchema = new mongoose.Schema({
     name: String,
+    category: Number,
     desc: String,
     stock: Number,
     rating: {
@@ -26,16 +27,17 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Product", productSchema);
 
-const gpu = new Product({
-    name: "MSI GeForce RTX 3080 VENTUS 3X PLUS 10G OC LHR",
-    desc: "Graphics Card - 10GB GDDR6X (19000MHz), NVIDIA GeForce Ampere (GA102, 1440 MHz), Boost 1740 MHz, PCI Express x16 4.0 , 320Bit, DisplayPort 1.4a and HDMI 2.1, LHR (Low Hash Rate)",
-    stock: 4,
+const newProd = new Product({
+    name: "Kingston FURY 16 GB KIT DDR4 3200 MHz CL16 Beast Black",
+    category: 3,
+    desc: "Operačná pamäť - 2x8GB, PC4-25600, CL16-18-18, napätie 1.35V, pasívny chladič, XMP 2.0 a Single Rank",
+    stock: 30,
     rating: 5,
-    price: 896.90,
-    img: "/images/MSI GeForce RTX 3080 VENTUS 3X PLUS 10G OC LHR.jfif"
+    price: 57.90,
+    img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=DEb16b32b2"
 });
 
-gpu.save();
+//newProd.save();
 
 
 
