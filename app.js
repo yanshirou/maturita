4fd2bba86dc5f6b6c5a58b490aec623ab9e300f5
Yanshiro <peter.michalik230@gmail.com>
2022-12-01 02:18:16 +0100

    add login system v1

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+// cookies shit
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(session({
+    secret: "Toto potom treba dat do .env",
+    resave: false,
+    saveUninitialized: false // Toto potom checkni neviem teraz
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 const uri = "mongodb+srv://admin-yanshiro:" + process.env.DATABASE_PASSWORD + "@cluster0.bwelaio.mongodb.net/eshopDB"
 mongoose.connect(uri);
@@ -27,19 +38,24 @@ const productSchema = new mongoose.Schema({
     img: String
 })
 
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+})
+
+userSchema.plugin(passportLocalMongoose);
+
 const Product = mongoose.model("Product", productSchema);
 
-const newProd = new Product({
-    name: "Kingston FURY 16 GB KIT DDR4 3200 MHz CL16 Beast Black",
-    category: 3,
-    desc: "Operačná pamäť - 2x8GB, PC4-25600, CL16-18-18, napätie 1.35V, pasívny chladič, XMP 2.0 a Single Rank",
-    stock: 30,
-    rating: 5,
-    price: 57.90,
-    img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=DEb16b32b2"
-});
+const User = mongoose.model("User", userSchema);
 
-//newProd.save();
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 
 // ROUTES
@@ -73,6 +89,54 @@ app.get("/newprod", (req, res) => {
     res.render("newprod");
 })
 
+    // LOGIN AND REGISTER PAGE
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.get("/profile", (req, res) => {
+    if (req.isAuthenticated()){
+        res.render("profile", {acc: req.user.username})
+    } else {
+        res.redirect("/login");
+    }
+})
+
+app.post("/register", (req, res) => {
+    User.register({username: req.body.username, email: req.body.email}, req.body.password, (err, newUser) => {
+        if(!err){
+            passport.authenticate("local")(req, res, () => {
+                // console.log(req.user);
+                res.redirect("/profile")
+            })
+        } else {
+            console.log(err);
+            res.redirect("/register")
+        }
+    })
+})
+
+app.post("/login", (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if(!err) {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/profile");
+            })
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+
 app.post("/newprod", (req, res) => {
     const newProd = new Product({
         name: req.body.newProdName,
@@ -92,6 +156,7 @@ app.post("/newprod", (req, res) => {
         }
     });
 })
+
 
 
 app.listen(3000, () => {
